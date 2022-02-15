ffc38522b192ca87a8f13661feb5251361270c2d
Jumar Macato <16554748+jmacato@users.noreply.github.com>
2022-02-15 23:11:24 +0800

    From Synfonia to Xune
    Co-authored-by: Dan Walmsley <dan@walms.co.uk>

---
+++ iTunesSearch/iTunesSearch.Library/Models/AlbumResult.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iTunesSearch.Library.Models
+{
+    [DataContract]
+    public class AlbumResult
+    {
+        [DataMember(Name = "resultCount")]
+        public int Count { get; set; }
+
+        [DataMember(Name = "results")]
+        public List<Album> Albums { get; set; }
+    }
+}
